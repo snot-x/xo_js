d7ce883a0fd9ad7b77e3e10a7b2566f0699795f3
snot-x <snot@humanoid.net>
2025-08-24 22:01:22 -0300

    got lost in the sauce and coded the round logic and started the playGame function. have hit a bit of a speedbump at the loop

--- xo.js
+++ xo.js
@@ -6,11 +6,11 @@ function getComputerChoice(params) {
   let choice = Math.random();
 
   if (choice < 0.33) {
-    computerChoice = "Rock";
+    computerChoice = "rock";
   } else if (choice > 0.66) {
-    computerChoice = "Scissors";
+    computerChoice = "scissors";
   } else {
-    computerChoice = "Paper";
+    computerChoice = "paper";
   }
   console.log(computerChoice);
   return computerChoice;
@@ -22,9 +22,31 @@ function getHumanChoice() {
   return hmnChoice.toLowerCase(); // so that the input is uniformly lowercase
 }
 
-getComputerChoice();
-getHumanChoice();
 
-function playRound(hmnChoice, getComputerChoice) {
-  
+function playRound(hmnChoice, computerChoice) {
+  hmnChoice.toLowerCase();
+  if (hmnChoice === computerChoice) {
+    console.log("Tie");
+  } else if (hmnChoice === "rock") {
+    if (computerChoice === "paper") {
+      console.log("paper beats rock hmn loss");
+    } else {
+      console.log("human wins")
+    }
+  } else if (hmnChoice === "scissors") {  
+    if (computerChoice === "rock") {
+      console.log("You lose. Rock beats scissors.")
+    } else {
+      console.log("You win, scissors cut paper");
+    }
+  } else if (hmnChoice === "paper") {
+    if (computerChoice === "scissors") {
+      console.log("You lose, scissors cut paper");
+    } else {
+      console.log("You win! paper covers rock")
+    }
+  }
 }
+
+const hmnSelection = getHumanChoice();
+const cmpSelection = getComputerChoice();
